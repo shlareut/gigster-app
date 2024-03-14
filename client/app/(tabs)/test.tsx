1add7b28c1b419544785b5bd73aa30cd9ac1e809
Lars Reuter <lp.reuter@gmail.com>
2024-03-14 20:06:09 +0100

    Update listing card design

--- client/app/(tabs)/test.tsx
+++ client/app/(tabs)/test.tsx
@@ -1,44 +1,53 @@
 import { router, useFocusEffect } from 'expo-router';
 import LottieView from 'lottie-react-native';
 import { useState } from 'react';
-import { Image, Pressable, Text, View } from 'react-native';
+import { Image, ImageBackground, Pressable, Text, View } from 'react-native';
 import checkLoginStatus from '../../util/sessions';
 import CustomButton from '../components/CustomButton';
+import { nextHost } from '../constants';
 
-export default function ProfileScreen() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userID, setUserId] = useState(null);
-  useFocusEffect(() => {
-    checkLoginStatus().then((status) => {
-      setIsLoggedIn(status.isLoggedIn);
-      setUserId(status.userId);
-      console.log('LOGIN STATUS:', status.isLoggedIn);
-    });
-  });
+export default function TestScreen() {
   return (
-    <View className="flex-1 h-screen w-screen items-center justify-center bg-white">
-      <LottieView
-        style={{ width: '25%', height: '25%' }}
-        source={require('../../assets/loadingAnimation.json')}
-        autoPlay
-        loop
-      />
-      <Text>Loading...</Text>
-      <LottieView
-        style={{ width: '25%', height: '25%' }}
-        source={require('../../assets/buttonLoadingAnimation.json')}
-        autoPlay
-        loop
-      />
-      <Pressable
-        className="w-fit justify-center items-center rounded-md bg-cyan-800 opacity-50"
-        disabled={true}
-      >
-        <Text className="my-2 mx-5 font-extrabold text-white text-lg">
-          .....
-        </Text>
-      </Pressable>
-      <Text> Hello</Text>
-    </View>
+    <>
+      {/* // LISTING CARD */}
+      <View className="w-11/12 my-3 self-center">
+        <View className="w-full h-auto aspect-square">
+          <Image
+            className="flex-1 rounded-3xl"
+            source={{
+              uri: `${nextHost}/hero_images/1.jpeg`,
+            }}
+          />
+        </View>
+        <View className="mt-3">
+          <Text className="text-xl font-bold">Café Menta</Text>
+          <Text className="text-base">Café in Landstraße, Vienna</Text>
+          <Text className="text-base underline">
+            <Text className="font-bold">15.50 EUR</Text> per hour
+          </Text>
+        </View>
+      </View>
+      {/* BOOKING CARD */}
+      <View className="w-11/12 my-3 self-center">
+        <View className="items-center h-fit flex-row">
+          <View className="h-24 w-24 mr-3">
+            <Image
+              className="flex-1 rounded-xl"
+              source={{
+                uri: `${nextHost}/hero_images/1.jpeg`,
+              }}
+            />
+          </View>
+          <View className="flex-1 gap-0.5">
+            <Text className="text-base font-bold">Option name</Text>
+            <Text>Listing name</Text>
+            <Text className="text-xs text-gray-500">Applied on date</Text>
+            <View className="pt-1">
+              <Text>Status</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    </>
   );
 }
--- client/app/components/ListingCard.tsx
+++ client/app/components/ListingCard.tsx
@@ -1,10 +1,52 @@
 import { router } from 'expo-router';
 import React from 'react';
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native';
+import {
+  Image,
+  ImageBackground,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { nextHost } from '../constants';
 
 const ListingCard = (props) => {
   return (
+    // <TouchableOpacity
+    //   activeOpacity={0.75}
+    //   onPress={() => {
+    //     router.navigate({
+    //       pathname: '../(stack)/[details]',
+    //       params: { id: props.listing.id },
+    //     });
+    //   }}
+    // >
+    //   <View className="my-3 w-11/12 self-center">
+    //     <View className="h-40 border-x border-t rounded-t-3xl">
+    //       <ImageBackground
+    //         source={{ uri: `${nextHost}/hero_images/${props.listing.id}.jpeg` }}
+    //         className="flex-1"
+    //         style={{
+    //           borderTopLeftRadius: '25',
+    //           borderTopRightRadius: '25',
+    //           overflow: 'hidden',
+    //         }}
+    //       >
+    //         <View className="rounded-xl w-fit h-fit mr-5 mt-5 border bg-white self-end">
+    //           <Text className="mx-4 my-2 font-bold">NEW</Text>
+    //         </View>
+    //       </ImageBackground>
+    //     </View>
+    //     <View className="border rounded-b-3xl">
+    //       <View className="my-4 mx-3">
+    //         <Text className="text-3xl font-bold">{props.listing.name}</Text>
+    //         <Text className="mt-2 font-bold">{props.listing.type}</Text>
+    //         <Text className="mt-2">{props.listing.address_line_one}</Text>
+    //         <Text className="mt-2">{props.listing.city_district}</Text>
+    //       </View>
+    //     </View>
+    //   </View>
+    // </TouchableOpacity>
+    // NEW LISTING CARD
     <TouchableOpacity
       activeOpacity={0.75}
       onPress={() => {
@@ -14,29 +56,24 @@ const ListingCard = (props) => {
         });
       }}
     >
-      <View className="my-3 w-11/12 self-center">
-        <View className="h-40 border-x border-t rounded-t-3xl">
-          <ImageBackground
-            source={{ uri: `${nextHost}/hero_images/${props.listing.id}.jpeg` }}
-            className="flex-1"
-            style={{
-              borderTopLeftRadius: '25',
-              borderTopRightRadius: '25',
-              overflow: 'hidden',
+      <View className="w-11/12 my-5 self-center">
+        <View className="w-full h-auto aspect-square">
+          <Image
+            className="flex-1 rounded-3xl"
+            source={{
+              uri: `${nextHost}/hero_images/${props.listing.id}.jpeg`,
             }}
-          >
-            <View className="rounded-xl w-fit h-fit mr-5 mt-5 border bg-white self-end">
-              <Text className="mx-4 my-2 font-bold">NEW</Text>
-            </View>
-          </ImageBackground>
+          />
         </View>
-        <View className="border rounded-b-3xl">
-          <View className="my-4 mx-3">
-            <Text className="text-3xl font-bold">{props.listing.name}</Text>
-            <Text className="mt-2 font-bold">{props.listing.type}</Text>
-            <Text className="mt-2">{props.listing.address_line_one}</Text>
-            <Text className="mt-2">{props.listing.city_district}</Text>
-          </View>
+        <View className="mt-3">
+          <Text className="text-xl font-bold">{props.listing.name}</Text>
+          <Text className="text-base">
+            {props.listing.type} in {props.listing.city_district},{' '}
+            {props.listing.city}
+          </Text>
+          <Text className="text-base underline">
+            <Text className="font-bold">15.50 EUR</Text> per hour
+          </Text>
         </View>
       </View>
     </TouchableOpacity>
